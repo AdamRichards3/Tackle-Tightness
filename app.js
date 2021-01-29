a7862ade87d2a97c274dac17deead61fc7927ec2
Adam Richards <adamrichards@Adams-MacBook-Pro.local>
2021-01-29 16:19:36 +0000

    server

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 'use strict'
-
+// This make sure express is installed 
 const express = require('express');
 
 const app = express();
 
 const PORT  = process.env.PORT || 8080;
 
+app.use(express.static('public'));
+app.use(express.static('js'));
+
 app.listen(PORT, (err) => {
     console.log(`App listening on http://localhost:${PORT}`);
+});
+
+app.get('/',function(req,res) {
+    res.sendFile('index.html');
+});
+
+
+app.use(function (err, req, res, next){
+    console.log(err.stack)
+    res.status(500).send('An error has occured!')
 });

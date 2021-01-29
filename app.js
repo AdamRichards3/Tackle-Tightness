7aee6b20d441a3fd9b73dc89f0705944099f36bd
Adam Richards <adamrichards@Adams-MacBook-Pro.local>
2021-01-29 16:12:02 +0000

    Express added

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 'use strict'
 
-const express = 
+const express = require('express');
+
+const app = express();
+
+const PORT  = process.env.PORT || 8080;
+
+app.listen(PORT, (err) => {
+    console.log(`App listening on http://localhost:${PORT}`);
+});

04b0611c242de38fc1ce9c6053a5a2371e1aea4e
Adam Richards <adamwilliamrichards@gmail.com>
2021-02-08 13:28:29 +0000

    documentation

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 'use strict'
-// This make sure express is installed 
+// This make sure all the dependencies are installed 
 const express = require('express');
 const GoogleAuth = require('simple-google-openid');
 const mysql = require('mysql');
-
+// Calls express to be used
 const app = express();
-
+// States what the port is, can also be stored in a .env file
 const PORT  = process.env.PORT || 8080;
-
+// Folders express needs to see
 app.use(express.static('public'));
 app.use(express.static('js'));
-
+// Tells express what port to listne to 
 app.listen(PORT, (err) => {
     console.log(`App listening on http://localhost:${PORT}`);
 });
-
+// Makes a get request to the index.html file
 app.get('/',function(req,res) {
     res.sendFile('index.html');
 });
-
-
+// This will send a error message if there is status 500 
 app.use(function (err, req, res, next){
     console.log(err.stack)
     res.status(500).send('An error has occured!')
 });
-
+// This intilises the MySQL coonection 
 const mysqlConnection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
@@ -32,7 +31,7 @@ const mysqlConnection = mysql.createConnection({
     database : 'Exercise',
     multipleStatements : true
 })
-
+// this checks if the connection has happened
 mysqlConnection.connect((err) => {
     if(err)
     {
@@ -40,11 +39,11 @@ mysqlConnection.connect((err) => {
     }
     console.log('Database ' + mysqlConnection.state);
 })
-
+// The google client code 
 app.use(GoogleAuth('1061299997929-s6ts70cla6voe6g6k31pdp0oj6hrfhll.apps.googleusercontent.com'));
-
+// installs the google auth middle ware
 app.use('/api', GoogleAuth.guardMiddleware());
-
+// Will display the users information
 app.get('/api/hello', (req, res) => {
     res.send('Hello ' + (req.user.displayName || 'user without a name') + '!');
     console.log('successful authenticated request by ' + req.user.emails[0].value);

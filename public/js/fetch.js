a84e3f884dbc768420b660b1931e16ef8619e67a
Adam Richards <adamwilliamrichards@gmail.com>
2021-03-04 14:33:25 +0000

    geeting the data on the page

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,3 +1,4 @@
+ 'use strict'
  async function searchHandler() {
   let userInput = document.getElementById("dropDown").value;
 
@@ -9,9 +10,35 @@
   console.log(await getExercises.text());
 }
 
+async function displayExercises(){
+  let userInput = document.getElementById("dropDown").value;
+
+  const options = {
+    method: 'GET'
+  };
+
+  let fetchExercises = await fetch('/loadExercises/' + userInput, options);
+  return await fetchExercises;
+}
+
+function getData(){
+  const loadExercises = displayExercises();
+  console.log(loadExercises)
+  const body = document.getElementById('exercise-grid');
+  console.log(body);
+  
+  const name = document.createElement('h2');
+  name.className = "name";
+  name.textContent = displayExercises.text;
+  body.appendChild(name);
+
+  const description = document.createElement('h2');
+  description.className = "description";
+  body.appendChild(description);
+}
+
 
 window.onload = function () {
-  console.log("loaded");
-  console.log(document.getElementsByClassName("submit")[0]);
   document.getElementsByClassName("submit")[0].addEventListener("click", searchHandler);
+  getData();
 }

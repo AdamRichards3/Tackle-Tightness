b9b4bacb33e4ddbaacfbf3cb0051d23818409244
Adam Richards <adamwilliamrichards@gmail.com>
2021-03-04 17:37:53 +0000

    added is showing after submit

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,5 +1,6 @@
  'use strict'
  async function searchHandler() {
+  clearSearchResult();
   let userInput = document.getElementById("dropDown").value;
 
   const options = {
@@ -8,6 +9,7 @@
 
   let getExercises = await fetch('/loadExercises/' + userInput, options);
   console.log(await getExercises.text());
+  getData();
 }
 
 async function displayExercises(){
@@ -18,27 +20,53 @@ async function displayExercises(){
   };
 
   let fetchExercises = await fetch('/loadExercises/' + userInput, options);
-  return await fetchExercises;
+  let test = await fetchExercises.json();
+  console.log(test);
+
+  return test;
 }
 
-function getData(){
-  const loadExercises = displayExercises();
+async function getData(){
+  const loadExercises = await displayExercises();
   console.log(loadExercises)
-  const body = document.getElementById('exercise-grid');
-  console.log(body);
-  
-  const name = document.createElement('h2');
-  name.className = "name";
-  name.textContent = displayExercises.text;
-  body.appendChild(name);
+  console.log(loadExercises[1]);
 
-  const description = document.createElement('h2');
-  description.className = "description";
-  body.appendChild(description);
+  const gridEle = document.getElementById('exercise-grid');
+
+  for (let i = 0; i < loadExercises.length; i++){
+
+    let parentEle = document.createElement("div");
+    parentEle.className = "exercise-result";
+    console.log(parentEle);
+    
+    const name = document.createElement('h2');
+    name.className = "exercise-name";
+    name.textContent = loadExercises[i].exercise_name;
+    parentEle.appendChild(name);
+    
+    const description = document.createElement('h2');
+    description.textContent = loadExercises[i].exercise_description;
+    description.className = "exercise-description";
+    parentEle.appendChild(description);
+
+    gridEle.appendChild(parentEle);
+  }
+}
+
+function clearSearchResult(){
+  let parentEle = document.getElementById("exercise-grid");
+  let childrenEle = parentEle.querySelectorAll(".exercise-result");
+
+  console.log(parentEle);
+  console.log(childrenEle);
+  for(let i =0; i < childrenEle.length; i++){
+      parentEle.removeChild(childrenEle[i]);
+  }
 }
 
 
 window.onload = function () {
   document.getElementsByClassName("submit")[0].addEventListener("click", searchHandler);
-  getData();
+  document.getElementsByClassName("clear")[0].addEventListener("click", clearSearchResult);
+  
 }
